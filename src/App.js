a6358aebb50d4f9eae57cc9d8742c3bc9fb4e228
unknown <dreamdehradun@gmail.com>
2021-03-31 21:18:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="page-container">
+      <div className="content-wrap">
+        
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer class="main" role="contentinfo" data-swiftype-index="false">
+    <nav class="menu social-media-menu">
+      <div class="social">
+        <h5>Follow us on</h5>
+        <ul class="links"><li class="menu-193268 first"><a href="https://supr.cl/2FyB6q2" class="youtube">YouTube</a></li>
+  <li class="menu-922"><a href="https://supr.cl/353IhiL" title="Supercell on Facebook" class="facebook" target="_blank">Facebook</a></li>
+  <li class="menu-193269"><a href="https://supr.cl/3j8f2Ai" class="instagram">Instagram</a></li>
+  <li class="menu-923"><a href="http://supr.cl/2pzzIsf" title="Supercell on Twitter" class="twitter" target="_blank">Twitter</a></li>
+  <li class="menu-192777"><a href="https://supr.cl/2T3yYJS" title="Supercell on LinkedIn" class="linkedin">LinkedIn</a></li>
+  <li class="menu-193270 last"><a href="https://supr.cl/2H81F5R" class="glassdoor">Glassdoor</a></li>
+  </ul>    </div>
+      <div class="app-stores">
+        <h5>Download our games from</h5>
+        <a href="https://supr.cl/appstorescgames" class="appstore">App Store</a>
+        <a href="https://play.google.com/store/apps/dev?id=6715068722362591614&amp;hl=en" class="googleplay">Google Play</a>
+      </div>
+    </nav>
+  
+    <nav class="menu secondary secondary-menu">
+      <ul id="footer-menu" class="links clearfix"><li class="menu-850 first"><a href="/en/terms-of-service/">Terms of Service</a></li>
+  <li class="menu-851"><a href="/en/privacy-policy/">Privacy Policy</a></li>
+  <li class="menu-1602"><a href="/en/parents/">Parent's Guide</a></li>
+  <li class="menu-76940 last"><a href="/en/safe-and-fair-play/">Safe and Fair Play Policy</a></li>
+  </ul>  </nav>
+  
+    <div class="footer-end">
+      <div class="mail">
+        <address>
+          <span>Supercell Oy</span>
+          <span>Jätkäsaarenlaituri 1</span>
+          <span>00180 Helsinki</span>
+          <span>Finland</span>
+        </address>
+      </div>
+      <div class="logo">
+        <a href="/en/" title="Front page">Supercell</a>
+      </div>
+    </div>
+  </footer>
+  );
+}
+
+export default Footer;
